ec0d687edb8914abb4ba5b6a5c8b0fd5842f5de2
jitendra-shinde8982 <jitushinde8982@gmail.com>
2023-12-29 12:37:34 +0530

    first commit

---
+++ asset/js/index.js
@@ -0,0 +1,19 @@
+$("#openaddress").hide();
+
+
+function iconhide_1(){
+    $("#opendate1").hide();
+}
+function iconhide_2(){
+    $("#opendate2").hide();
+}
+function showAdd() {
+    $("#openaddress").show();
+}
+function Createprofile(){
+    $("#createProfile").show();
+    $("#closeProfile").hide();
+}
+ function loginBtn(){
+    window.location.href = "forms/profile_screen_1.index.html";
+ }
